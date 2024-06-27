458f1ab7d6b75389d00782e429c50d7b92d53674
Sahil <sahilnale@icloud.com>
2024-06-27 12:30:46 -0700

    new

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Upgrade pip, setuptools, and wheel
 pip install --upgrade pip setuptools wheel
 
-# Install dependencies
+# Install numpy first
+pip install numpy==1.19.5
+
+# Install remaining dependencies
 pip install cython
 pip install -r requirements.txt
