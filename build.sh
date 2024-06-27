b0523071819eae5767ee515e10455c00a6b9a823
Sahil <sahilnale@icloud.com>
2024-06-27 12:32:36 -0700

    n

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Upgrade pip, setuptools, and wheel
-pip install --upgrade pip setuptools wheel
+pip install --upgrade pip
 
 # Install numpy first
 pip install numpy==1.19.5
